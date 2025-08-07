84e2466bcd6c062777b47caae674ca5d6807862c
Bryce Benedetto <bryce.benedetto29@gmail.com>
2025-08-06 23:15:23 -0400

    cache fixes 3

--- core/cleaner/spotify_auth.py
+++ core/cleaner/spotify_auth.py
@@ -2,15 +2,15 @@ import os
 from dotenv import load_dotenv
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-
-load_dotenv()
+import uuid
 
 def get_spotify_oauth():
+    temp_cache = f"/tmp/spotify_cache_{uuid.uuid4()}"
     return SpotifyOAuth(
         client_id=os.getenv("SPOTIPY_CLIENT_ID"),
         client_secret=os.getenv("SPOTIPY_CLIENT_SECRET"),
         redirect_uri=os.getenv("SPOTIPY_REDIRECT_URI"),
-        scope = "playlist-read-private playlist-read-collaborative playlist-modify-public playlist-modify-private",
+        scope="playlist-read-private playlist-modify-private playlist-modify-public",
         show_dialog=True,
-        cache_path=None
+        cache_path=temp_cache
     )
